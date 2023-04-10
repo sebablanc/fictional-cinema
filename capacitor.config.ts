d6a26fcd5940f5ff4f9a8c337ff4666e136c8003
sebablanc <blancseba@gmail.com>
2023-04-10 15:23:15 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fictional-cinema',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
